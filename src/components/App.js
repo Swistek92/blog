f8eaeff67f88b5987a738d559f223b6d00e4c893
Piotr Świstowski <swistekxd@gmail.com>
2021-11-21 05:35:41 +0100

    1

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
-import React from "react";
-import PostList from "./PostList";
+import React from 'react';
+import PostList from './PostList';
 
-
-const App=()=> { 
-  return(
+const App = () => {
+  return (
     <div className="ui container">
-      
-      <PostList/>
-    
+      <PostList />
     </div>
-  )
-}
+  );
+};
+
 export default App;
